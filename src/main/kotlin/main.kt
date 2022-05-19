e69f4698b5cd43f592bd65095c4122e5756de3ff
prince-tagaev <asaba@internet.ru>
2022-05-19 23:24:57 +0300

    added checkTransferAmountWithCard and checkTransferAmountWithVkPay

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main(){
-    println(transfer("Visa", amountTransefer = 20000F))
+    println(transfer("Vk pay", amountTransfer = 15000F))
 }
 
 fun transferFee(cardType: String = "Vk pay", amountTransefer: Float): Float {
@@ -10,8 +10,48 @@ fun transferFee(cardType: String = "Vk pay", amountTransefer: Float): Float {
     }
     return fee
 }
-fun transfer( cardType: String, amountPreviousTransfers:Float = 0F, amountTransefer: Float): String {
-    var fee = transferFee( cardType = cardType, amountTransefer = amountTransefer)
-    var lastTransfer = amountPreviousTransfers + amountTransefer
-    return ("Сумма на перевод - ${amountTransefer}, комиссия - ${fee}, снимается - ${amountTransefer + fee}, Ваш последний перевод - ${lastTransfer}").toString()
+fun transfer( cardType: String, amountPreviousTransfers:Float = 0F, amountTransfer: Float): String {
+    val  cards = arrayListOf<String>("Visa", "Мир", "Mastercard", "Maestro")
+    val maxTransferAtOnce: Float = 15_000F
+    val maxTransferPerMonthWithVkPay: Float = 40_000F
+        var transfer = if (cardType in cards){
+            return checkTransferAmountWithCard(amountTransfer = amountTransfer, cardType = cardType, amountPreviousTransfers = amountPreviousTransfers)
+        }else{
+            return checkTransferAmountWithVkPay(amountTransfer = amountTransfer, cardType = cardType, amountPreviousTransfers = amountPreviousTransfers)
+        }
+    return transfer.toString()
+}
+
+fun checkTransferAmountWithCard(amountTransfer: Float, cardType: String, amountPreviousTransfers: Float = 0F): String{
+    val  cards = arrayListOf<String>("Visa", "Мир", "Mastercard", "Maestro")
+    val maxTransferPerMonthWithCard: Float = 600_000F
+    val maxTransferPerDayWithCard: Float = 150_000F
+    var checkAmount = if (amountTransfer > maxTransferPerDayWithCard && cardType in cards && amountTransfer < maxTransferPerMonthWithCard){
+        return ("Ваш лимит ограничен! Вы можете перевести ${maxTransferPerDayWithCard} рублей в день.")
+    }else if ((amountTransfer+amountPreviousTransfers) > maxTransferPerMonthWithCard  && cardType in cards){
+        return ("Ваш лимит ограничен! Вы можете перевести ${maxTransferPerMonthWithCard} рублей в месяц.")
+    }else{
+        var fee = transferFee( cardType = cardType, amountTransefer = amountTransfer)
+        return (("Сумма на перевод - ${amountTransfer}, комиссия - ${fee}," +
+                "снимается - ${amountTransfer + fee}, Ваш последний перевод - ${amountPreviousTransfers}").toString())
+    }
+    amountPreviousTransfers + amountTransfer
+    return checkAmount.toString()
+}
+
+fun checkTransferAmountWithVkPay(amountTransfer: Float, cardType: String, amountPreviousTransfers: Float = 0F): String{
+    val  cards = arrayListOf<String>("Vk pay")
+    val maxTransferAtOnce: Float = 15_000F
+    val maxTransferPerMonthWithVkPay: Float = 40_000F
+    var checkAmount = if (amountTransfer > maxTransferAtOnce && cardType in cards && amountTransfer < maxTransferPerMonthWithVkPay){
+        return ("Ваш лимит ограничен! Вы можете перевести ${maxTransferAtOnce} рублей в день.")
+    }else if ((amountTransfer+amountPreviousTransfers) > maxTransferPerMonthWithVkPay  && cardType in cards){
+        return ("Ваш лимит ограничен! Вы можете перевести ${maxTransferPerMonthWithVkPay} рублей в месяц.")
+    }else{
+        var fee = transferFee( cardType = cardType, amountTransefer = amountTransfer)
+        return (("Сумма на перевод - ${amountTransfer}, комиссия - ${fee}," +
+                "снимается - ${amountTransfer + fee}, Ваш последний перевод - ${amountPreviousTransfers}").toString())
+    }
+    amountPreviousTransfers + amountTransfer
+    return checkAmount.toString()
 }
